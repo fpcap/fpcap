cf318442fc7e68e94a1fa0e14c281d36f0037a48
Schwaggot <tom.ryan@posteo.de>
2022-03-29 13:01:23 +0200

    Extended metadata example

--- examples/example-metadata.cpp
+++ examples/example-metadata.cpp
@@ -7,26 +7,26 @@ using namespace std;
 using namespace std::chrono;
 
 int main() {
-    string filepath = "tracefiles/pcapng-example.pcapng";
+    for (const string& filepath : {"tracefiles/pcapng-example.pcapng", "tracefiles/many_interfaces-1.pcapng"}) {
+        mmpr::MMPcapNgReader reader(filepath);
+        reader.open();
 
-    mmpr::MMPcapNgReader reader(filepath);
-    reader.open();
-
-    mmpr::Packet packet{};
-    uint64_t processedPackets{0};
-    while (!reader.isExhausted()) {
-        if (reader.readNextPacket(packet)) {
-            processedPackets++;
+        mmpr::Packet packet{};
+        uint64_t processedPackets{0};
+        while (!reader.isExhausted()) {
+            if (reader.readNextPacket(packet)) {
+                processedPackets++;
+            }
         }
+
+        cout << fmt::format("Metadata.Comment:  {}", reader.getComment()) << endl;
+        cout << fmt::format("Metadata.OS:       {}", reader.getOS()) << endl;
+        cout << fmt::format("Metadata.Hardware: {}", reader.getHardware()) << endl;
+        cout << fmt::format("Metadata.UserAppl: {}", reader.getUserApplication()) << endl;
+
+        // close file descriptor and unmap memory
+        reader.close();
     }
-
-    cout << fmt::format("Metadata.Comment:  {}", reader.getComment()) << endl;
-    cout << fmt::format("Metadata.OS:       {}", reader.getOS()) << endl;
-    cout << fmt::format("Metadata.Hardware: {}", reader.getHardware()) << endl;
-    cout << fmt::format("Metadata.UserAppl: {}", reader.getUserApplication()) << endl;
-
-    // close file descriptor and unmap memory
-    reader.close();
 
     return 0;
 }
