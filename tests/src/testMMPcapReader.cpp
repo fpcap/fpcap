c907246157d90fa6926ef659052f221dd9784829
Schwaggot <tom.ryan@posteo.de>
2022-04-04 18:13:36 +0200

    Added simple MMPcapReader constructor unit test

---
+++ tests/src/testMMPcapReader.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include <mmpr/MMPcapReader.h>
+
+TEST(MMPcapReader, ConstructorSimple) {
+    mmpr::MMPcapReader reader{"tracefiles/example.pcap"};
+    EXPECT_EQ(reader.getFilepath(), "tracefiles/example.pcap")
+        << "Hint: make sure to execute unit tests from root directory";
+}
+
+TEST(MMPcapReader, ConstructorMissingFile) {
+    EXPECT_THROW(mmpr::MMPcapReader{"missing-file"}, std::runtime_error);
+}
+
+TEST(MMPcapReader, FaultyConstructor) {
+    EXPECT_THROW(mmpr::MMPcapReader{nullptr}, std::logic_error);
+    EXPECT_THROW(mmpr::MMPcapReader{""}, std::runtime_error);
+}
