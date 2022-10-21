b03a7aea34840801f98bf90d107da441998e975f
Schwaggot <tom.ryan@posteo.de>
2022-10-21 22:58:39 +0200

    Added RAII wrapper around Windows HANDLE

--- include/mmpr/filesystem/reading/MMapFileReader.hpp
+++ include/mmpr/filesystem/reading/MMapFileReader.hpp
@@ -10,15 +10,54 @@
 #include <filesystem>
 #include <stdexcept>
 #if __linux__
-#include <unistd.h>
 #include <sys/mman.h>
+#include <unistd.h>
 #elif _WIN32
-#include <winsock2.h>
 #include <memoryapi.h>
+#include <winsock2.h>
 #include <windows.h>
 #endif
 
 namespace mmpr {
+
+#if _WIN32
+// Windows HANDLEs are not RAII compatible, therefore we need to wrap them
+// See https://stackoverflow.com/a/34405788
+class WinHandle {
+public:
+    WinHandle(std::nullptr_t = nullptr) : value_(nullptr) {}
+    WinHandle(HANDLE value) : value_(value == INVALID_HANDLE_VALUE ? nullptr : value) {}
+
+    explicit operator bool() const { return value_ != nullptr; }
+    operator HANDLE() const { return value_; }
+
+    friend bool operator==(WinHandle l, WinHandle r) { return l.value_ == r.value_; }
+    friend bool operator!=(WinHandle l, WinHandle r) { return !(l == r); }
+
+    struct Deleter {
+        typedef WinHandle pointer;
+        void operator()(WinHandle handle) const { CloseHandle(handle); }
+    };
+
+private:
+    HANDLE value_;
+};
+
+inline bool operator==(HANDLE l, WinHandle r) {
+    return WinHandle(l) == r;
+}
+inline bool operator!=(HANDLE l, WinHandle r) {
+    return !(l == r);
+}
+inline bool operator==(WinHandle l, HANDLE r) {
+    return l == WinHandle(r);
+}
+inline bool operator!=(WinHandle l, HANDLE r) {
+    return !(l == r);
+}
+
+typedef std::unique_ptr<WinHandle, WinHandle::Deleter> HandlePtr;
+#endif
 
 class MMapFileReader : public FileReader {
 public:
@@ -44,24 +83,27 @@ public:
         mOffset = 0;
         mMappedMemory = reinterpret_cast<const uint8_t*>(mmapResult);
 #elif _WIN32
-        mFileHandle = CreateFile(mFilepath.c_str(), GENERIC_READ,
-                              /*shared mode*/ 0,
-                              /*security*/ nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL,
-                              /*template*/ nullptr);
-        mFileMappingHandle = CreateFileMapping(mFileHandle, NULL, PAGE_READONLY, /*size high*/ 0,
-                                           /*size low*/ 0, NULL);
-
-        if (!mFileMappingHandle) {
-            CloseHandle(mFileHandle);
-            throw std::runtime_error("!hInputMap");
+        mFileHandle = HandlePtr(CreateFile(mFilepath.c_str(), GENERIC_READ,
+                                           /*shared mode*/ 0,
+                                           /*security*/ nullptr, OPEN_EXISTING,
+                                           FILE_ATTRIBUTE_NORMAL,
+                                           /*template*/ nullptr));
+        if (!mFileHandle) {
+            throw std::runtime_error("could not create file handle for " + mFilepath);
         }
 
-        /* Map the input file */
-        auto mmapResult = MapViewOfFile(mFileMappingHandle, FILE_MAP_READ, 0, 0, 0);
+        mFileMappingHandle = HandlePtr(CreateFileMapping(mFileHandle.get(), NULL,
+                                                         PAGE_READONLY, /*size high*/ 0,
+                                                         /*size low*/ 0, NULL));
+        if (!mFileMappingHandle) {
+            throw std::runtime_error("could not create file mapping handle for " +
+                                     mFilepath);
+        }
+
+        // map the input file
+        auto mmapResult = MapViewOfFile(mFileMappingHandle.get(), FILE_MAP_READ, 0, 0, 0);
         if (mmapResult == nullptr) {
-            CloseHandle(mFileMappingHandle);
-            CloseHandle(mFileHandle);
-            throw std::runtime_error("ptrInFile == nullptr");
+            throw std::runtime_error("failed to map file to memory: " + mFilepath);
         }
 
         mOffset = 0;
@@ -73,15 +115,12 @@ public:
     MMapFileReader(mmpr::ZstdFileReader reader);
 #endif
 
-    ~MMapFileReader() {
 #if __linux__
+    ~MMapFileReader() {
         munmap((void*)mMappedMemory, mFileSize);
         ::close(mFileDescriptor);
-#elif _WIN32
-        CloseHandle(mFileMappingHandle);
-        CloseHandle(mFileHandle);
-#endif
     }
+#endif
 
     const uint8_t* data() const override { return mMappedMemory; }
 
@@ -89,8 +128,8 @@ private:
 #if __linux__
     int mFileDescriptor{0};
 #elif _WIN32
-    HANDLE mFileHandle;
-    HANDLE mFileMappingHandle;
+    HandlePtr mFileHandle;
+    HandlePtr mFileMappingHandle;
 #endif
     const uint8_t* mMappedMemory{nullptr};
 };
