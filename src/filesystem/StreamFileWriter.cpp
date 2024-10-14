bce5261518a8245b6b75c7e5f9c60bff3b4ab33d
Schwaggot <tom.ryan@posteo.de>
2024-10-14 14:55:36 +0200

    Raise error if StreamFileWriter could not open stream

--- src/filesystem/StreamFileWriter.cpp
+++ src/filesystem/StreamFileWriter.cpp
@@ -5,6 +5,8 @@ namespace fpcap {
 StreamFileWriter::StreamFileWriter(const std::string& filepath)
     : FileWriter(filepath),
       mOutputFileStream(mFilepath) {
+    throw std::runtime_error(
+            "could not open output stream: " + std::string(strerror(errno)));
 }
 
 void StreamFileWriter::write(const uint8_t* data, const size_t size) {
